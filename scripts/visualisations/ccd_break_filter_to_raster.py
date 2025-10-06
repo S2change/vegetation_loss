8a40b72c5310e94b27d0ff6672d2563c16a95be2
saractn <156934905+saractn@users.noreply.github.com>
2025-10-06 21:39:46 +0100

    Update ccd_break_filter_to_raster.py

--- scripts/visualisations/ccd_break_filter_to_raster.py
+++ scripts/visualisations/ccd_break_filter_to_raster.py
@@ -218,7 +218,7 @@ def read_parquet_identify_breaks(filepath):
     mask = (df['x_coord'].values == df['x_coord'].shift(-1).values) & (df['y_coord'].values == df['y_coord'].shift(-1).values)
     df['is_break'] = mask.astype(int)
 
-    # special case: pixel with only 1 segment and tBreak != tEnd
+    # special case: pixel with only 1 segment and tBreak != tEnd (sara 6 out 2025)
     single_segment = df.groupby(['x_coord', 'y_coord']).filter(lambda g: len(g) == 1)
     condition = single_segment['tBreak'] != single_segment['tEnd']
     df.loc[condition.index[condition], 'is_break'] = 1
