5318b5087eb5de648ea16f225ea7063cd8246bea
domwelsh <domwelsh@gmail.com>
2025-10-07 11:00:05 -0700

    Added quick explanation of the file

--- scripts/data_exploration/parquet_columns.py
+++ scripts/data_exploration/parquet_columns.py
@@ -1,3 +1,8 @@
+"""
+Functions for directly seeing the CCD results that are in parquet files
+
+"""
+
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
