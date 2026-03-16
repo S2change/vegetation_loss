2e36d41b1cd6e9dc8418ff99ee49488daaea0732
Dominic Welsh <isa127909@isa.utl.pt>
2026-03-16 20:15:53 +0000

    Updated parse_and_sort_files() to handle None for min/max date filters

--- scripts/data_exploration/tifs_to_hdf5_to_tifs/CNCA_tifs_to_hdf5/hdf5_utils.py
+++ scripts/data_exploration/tifs_to_hdf5_to_tifs/CNCA_tifs_to_hdf5/hdf5_utils.py
@@ -15,7 +15,7 @@ def parse_and_sort_files(folder, min_date, max_date):
         if match:
             ts_ms = int(match.group(1))
             dt = datetime.fromtimestamp(ts_ms / 1000.0, timezone.utc).date()
-            if min_date <= dt <= max_date:
+            if (min_date is None or min_date <= dt) and (max_date is None or dt <= max_date):
                 file_metadata.append({
                     'filename': f,
                     'ordinal': dt.toordinal(),
